9dd6d1e142a7c68d35a3f6b3f3034f979ce927c1
Amirreza <hjrezi1999@gmail.com>
2022-03-27 12:05:10 +0430

    SET VIEW AND FIX DASHBOARD VIEW

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,20 @@
+<!-- Side Header Start -->
+<div class="side-header show">
+    <button class="side-header-close"><i class="zmdi zmdi-close"></i></button>
+    <!-- Side Header Inner Start -->
+    <div class="side-header-inner custom-scroll">
+
+        <nav class="side-header-menu" id="side-header-menu">
+            <ul>
+                <li><a href=""><i class="ti-home"></i> <span>داشبورد</span></a></li>
+                <li class="has-sub-menu"><a href="#"><i class="ti-package"></i> <span>ویترین</span></a>
+
+                    <ul class="side-header-sub-menu">
+                        <li><a href="#"><span>محصولات</span></a></li>
+                        <li><a href="#"><span>قراردادها</span></a></li>
+                    </ul>
+            </ul>
+        </nav>
+
+    </div><!-- Side Header Inner End -->
+</div><!-- Side Header End -->
--- routes/web.php
+++ routes/web.php
@@ -6,13 +6,21 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 | Web Routes
 |--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::prefix('admin')->namespace('Admin')->group(function(){
+
+    //داشبورد
+    Route::get('/', [App\Http\Controllers\Admin\DashboardController::class , 'index'])->name('admin.home');
+
+       //محصولات
+       Route::prefix('products')->group(function(){
+          Route::get('/', [App\Http\Controllers\Admin\ProductController::class , 'index'])->name('admin.products.index');
+          Route::get('/create', [App\Http\Controllers\Admin\ProductController::class , 'create'])->name('admin.products.create');
+          Route::post('/store', [App\Http\Controllers\Admin\ProductController::class , 'store'])->name('admin.market.store');
+          Route::get('/edit/{productCategory}', [App\Http\Controllers\Admin\ProductController::class , 'edit'])->name('admin.products.edit');
+          Route::put('/update/{productCategory}', [App\Http\Controllers\Admin\ProductController::class , 'update'])->name('admin.products.update');
+          Route::delete('/destroy/{productCategory}', [App\Http\Controllers\Admin\ProductController::class , 'destroy'])->name('admin.products.destroy');
+       });
+
+ });
